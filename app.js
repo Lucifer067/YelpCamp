4134179936fa98ca36333a6dcf86246d9b8a9899
Sumit Chaurasiya <41621751+Lucifer067@users.noreply.github.com>
2021-03-07 10:48:45 +0545

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const campgroundRoutes= require('./routes/campground');
 const reviewRoutes= require('./routes/reviews');
 const userRoutes= require('./routes/users');
 
-const dbURl= process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl= process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 const app = express();
 const router= express.Router();
